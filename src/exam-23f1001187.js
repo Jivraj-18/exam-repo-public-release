369eec3c5ddce9e4270b18d6b46ecb811228b33c
sainathshelke <sainathshelke06@gmail.com>
2025-12-17 14:17:04 +0530

    Add 5 TDS bonus questions from 23f1001187

--- src/exam-23f1001187.js
+++ src/exam-23f1001187.js
@@ -3,7 +3,7 @@ import { displayQuestions } from "./utils/display.js";
 export async function questions(user, elementMap) {
   const results = [
     ...(await import("./q-Table-Extraction.js").then(m => [m.default({ user })])),
-    ...(await import("./q-playwright-purpose.js").then(m => [m.default({ user })])),
+    ...(await import("./q-playwright--purpose.js").then(m => [m.default({ user })])),
     ...(await import("./q-GitHub-API.js").then(m => [m.default({ user })])),
     ...(await import("./q-google-sheetsHTML.js").then(m => [m.default({ user })])),
     ...(await import("./q-Wget Crawling (CLI).js").then(m => [m.default({ user })])),
