c627769d5ef0deb85d5240f8a0d15f76d454e5a2
Stardust <22f3001255@ds.study.iitm.a.cin>
2025-12-18 17:58:30 +0530

    Add 5 new TDS Questions for 22f3001255

--- src/exam-22f3001255.js
+++ src/exam-22f3001255.js
@@ -2,7 +2,7 @@ import { displayQuestions } from "./utils/display.js";
 
 export async function questions(user, elementMap) {
   const results = [
-    ...(await import("./q-github-action-debug.js").then(m => [m.default({ user })])),
+    ...(await import("./q-github-actions-debug.js").then(m => [m.default({ user })])),
     ...(await import("./q-llm-embeddings-choice.js").then(m => [m.default({ user })])),
     ...(await import("./q-data-prep-duckdb.js").then(m => [m.default({ user })])),
     ...(await import("./q-visualization-misleading.js").then(m => [m.default({ user })])),
