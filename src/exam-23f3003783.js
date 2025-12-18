7e2776e2ddf96e058c23bc0698f4305d0ae2d63b
Iftikar Alam <iftikaralam0016@gmail.com>
2025-12-18 20:22:05 +0530

    Remove q-sample.js from exam

--- src/exam-23f3003783.js
+++ src/exam-23f3003783.js
@@ -2,15 +2,7 @@ import { displayQuestions } from "./utils/display.js";
 
 export async function questions(user, elementMap) {
     const results = [
-        {
-            ...(await import("./q-sample.js").then((m) =>
-                m.default({
-                    user,
-                    weight: 1,
-                }),
-            )),
-            help: [],
-        },
+
         {
             ...(await import("./q-log-analysis.js").then((m) => m.default({ user, weight: 1 }))),
         },
