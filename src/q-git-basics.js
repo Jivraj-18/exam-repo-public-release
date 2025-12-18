060dee2a8a5a2a2d27272065ccc5f15c4fe347b7
serenade2400 <sam1244saumya@gmail.com>
2025-12-18 22:27:26 +0530

    Add 5 TDS bonus questions

---
+++ src/q-git-basics.js
@@ -0,0 +1,21 @@
+import { html } from "https://cdn.jsdelivr.net/npm/lit-html@3/lit-html.js";
+
+export default async function ({ user, weight = 1 }) {
+  const id = "q-git-current-branch";
+  const title = "Git: Current Branch";
+
+  const answer = "git branch --show-current";
+
+  const question = html`
+    <div class="mb-3">
+      <p>
+        Which Git command shows the <strong>name of the currently checked-out branch</strong>
+        without listing all branches?
+      </p>
+      <label for="${id}" class="form-label">Command:</label>
+      <input class="form-control" id="${id}" name="${id}" />
+    </div>
+  `;
+
+  return { id, title, weight, question, answer };
+}
