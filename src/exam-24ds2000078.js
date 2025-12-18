a136db8f12e267e183f78d87da1f3c919692517f
harshvk25 <24ds2000078@ds.study.iitm.ac.in>
2025-12-18 16:27:30 +0530

    Add HTTP caching behavior question

--- src/exam-24ds2000078.js
+++ src/exam-24ds2000078.js
@@ -1,12 +1,17 @@
 import qJsonSchemaValidation from "./q-json-schema-validation.js";
 import qTimezoneNormalization from "./q-timezone-normalization.js";
 import qFeatureLeakageDetection from "./q-feature-leakage-detection.js";
+import qHttpCachingBehavior from "./q-http-caching-behavior.js";
+
 
 const examQuestions = [
   qJsonSchemaValidation,      // 1st question
   qTimezoneNormalization,     // 2nd question
   qFeatureLeakageDetection,   // 3rd question
+  qHttpCachingBehavior,       // 4th question
 ];
 
+
 export default examQuestions;
+
 
